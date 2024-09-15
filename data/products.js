30976b5562ca8e94b543113dedb3dc2fbd7e8727
sudhir <sudhir1072004@gmail.com>
2024-09-15 18:26:51 +0530

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -12,7 +12,7 @@ export function getProduct(productId){
     return matchingProduct;
 }
 
-class product {
+class Product {
   id;
   image;
   name;
@@ -39,7 +39,7 @@ class product {
   };
 }
 
-class Clothing extends product{
+class Clothing extends Product{
   sizeChartLink;
 
   constructor(productDetails){
@@ -87,6 +87,36 @@ const object3 ={
 object3.method();
 */
 
+export let products = [];
+
+export function loadProducts(fun){
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if(productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    } );
+
+    console.log('load products');
+
+    fun();
+
+  })
+
+  xhr.open('GET','https://supersimplebackend.dev/products');
+  xhr.send();
+
+}
+
+loadProducts();
+
+
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -752,3 +782,4 @@ export const products = [
   }
   return new product(productDetails);
 } );
+ */
